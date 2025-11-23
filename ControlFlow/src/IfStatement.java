98fa848a0c35049cc38be1731fabc9b49917ebde
SobczakMateusz05 <sobczak.mateusz27@gmail.com>
2025-11-23 22:07:26 +0100

    feat: flow control, logical operator, If Statement

---
+++ ControlFlow/src/IfStatement.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class IfStatement {
+    final static Scanner scanner = new Scanner(System.in);
+
+    public static void temperature() {
+        float temperature = provideTemperature();
+
+        checkTemperature(temperature);
+    }
+
+    public static void income(){
+        int income = provideIncome() ;
+        boolean hasHighIncome = (income > 100_000);
+        String className = hasHighIncome ? "Higher Class" : "Normal Class" ;
+
+        System.out.println("has high income?: " + hasHighIncome);
+        System.out.println("class: " + className);
+    }
+
+    private static float provideTemperature() {
+        System.out.print("Please enter the temperature in Celsius: ");
+        return scanner.nextFloat();
+    }
+
+    private static void checkTemperature(float temperature) {
+        if (temperature > 30) { //  more than 30
+            System.out.println("It's a hot day");
+            System.out.println("Drink plenty of water");
+        } else if (temperature >= 20) { // between 20 nad 30
+            System.out.println("It's a nice day");
+        } else { // less than 20
+            System.out.println("It's cold");
+        }
+    }
+
+    private static int provideIncome(){
+        System.out.print("Please enter the annual income: ");
+        return scanner.nextInt();
+    }
+}
